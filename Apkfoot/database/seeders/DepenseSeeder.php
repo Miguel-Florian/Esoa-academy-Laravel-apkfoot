0a77ddbef0df7300974f723c1ef7c08531d4542a
Miguel Florian <migflor04@gmail.com>
2021-01-23 09:46:30 +0100

    create depenseResource

---
+++ Apkfoot/database/seeders/DepenseSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DepenseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        /* \App\Models\Depense::create(
+        [
+            'prix' => '100000',
+            'date' => '22/03/2019',
+        ],
+        [
+            'prix' => "200000",
+            'date' => '22/03/2018',
+        ]
+    );*/
+    }
+}
