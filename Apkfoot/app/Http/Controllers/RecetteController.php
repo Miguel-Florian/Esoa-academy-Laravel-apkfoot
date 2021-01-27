1be4d127a19a98c2ab9a3e18ae5ada0116cce3a5
Miguel Florian <migflor04@gmail.com>
2021-01-27 04:09:46 +0100

    update recette controller

--- Apkfoot/app/Http/Controllers/RecetteController.php
+++ Apkfoot/app/Http/Controllers/RecetteController.php
@@ -25,26 +25,16 @@ class RecetteController extends Controller
     }
 
     public function recetteByDate(Request $request) {
-        /*$db_host = "localhost";
+
+        $cle=0;
+        $db_host = "localhost";
         $db_name = "foot";
         $db_user = "root";
         $db_pass = "";
         $db = new PDO("mysql:host=$db_host:3306;dbname=$db_name", $db_user, $db_pass);
-        /*$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $sumByDate=$db->query("SELECT SUM(Prix) FROM recettes WHERE date=$request");
-        $sum=$sumByDate->fetch();
-        var_dump($sumByDate);*/
-        /*$prix = $db->query("SELECT SUM(Prix) FROM recettes WHERE date=$request"/*DB::table('recettes')->where('Prix','>', 0)->value('Prix'));
-            $prix->num_rows;
-            while($row=$db_prix->fetch_assoc()){
-                $result= $row['Prix'] ;
-                echo($result."\n");
-            }
-            $db_result->free() ;
-                    chercher pourquoi cela affiche 10k,
-                    et voir si c'est possible d'afficher
-                    d'autre prix et la somme de ces prix*/
-
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sum=$db->query("SELECT SUM(Prix) FROM recettes")->fetch();
+        echo('La recette du '.$request['Date'].' est de: '.$sum[$cle]." Fcfa\n ");
         return RecetteResource::collection(Recette::where("date", $request['Date'])->get());
 
 
