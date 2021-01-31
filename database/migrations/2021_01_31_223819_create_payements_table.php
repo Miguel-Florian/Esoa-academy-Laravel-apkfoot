01c0c73db469e51fe50f51795721511e09820026
Miguel Florian <migflor04@gmail.com>
2021-01-31 23:57:54 +0100

    Payement migration

---
+++ database/migrations/2021_01_31_223819_create_payements_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePayementsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payements', function (Blueprint $table) {
+            $table->id();
+            $table->int("id_session");
+            $table->int("id_stagiaire");
+            $table->Date("date_payement");
+            $table->float("montant");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payements');
+    }
+}
