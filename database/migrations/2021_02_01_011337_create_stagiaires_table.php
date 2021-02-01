3c2e2950c653838279436ab92b7f1ae1fb287842
Miguel Florian <migflor04@gmail.com>
2021-02-01 02:45:49 +0100

    create Stagiaire migration

---
+++ database/migrations/2021_02_01_011337_create_stagiaires_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStagiairesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stagiaires', function (Blueprint $table) {
+            $table->id();
+            $table->integer("id_user");
+            $table->string("nom");
+            $table->string("prenom");
+            $table->string("adresse");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stagiaires');
+    }
+}
